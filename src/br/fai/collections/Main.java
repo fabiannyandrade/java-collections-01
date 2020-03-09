f18fe6715d36efb574e7d49c81c42b6d5835ecd2
Matuk <mateusmatuk@fai-mg.br>
2020-03-09 20:59:37 -0300

    segundo commit

--- src/br/fai/collections/Main.java
+++ src/br/fai/collections/Main.java
@@ -1,5 +1,9 @@
 package br.fai.collections;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -9,10 +13,52 @@ public class Main {
 
 	}
 
+	private Scanner scanner = new Scanner(System.in);
+	
+	private final String VALOR_INVALIDO = "VALOR_INVALIDO";
+	
 	private void start() {
 		
-		System.out.println(":3");
+		List<String> lista = new ArrayList<String>();
 		
+		
+		while(lista.size() < 5) {
+			String valorRecebido = obterDados();
+			if(valorRecebido.isEmpty()|| valorRecebido.equals(VALOR_INVALIDO)) {
+				System.out.println("Este valor foi descartado");
+			} else {
+			lista.add(valorRecebido);
+			}
+		}
+		System.out.println("---------");
+		for(String item: lista) {
+			System.out.println("O item da lista e:" + item);
+		}
+		
+		lista.add("item adicionado automaticamente 01");
+		lista.add("item adicionado automaticamente 02");
+		
+		lista.remove(1);
+		//lista.remove(1);
+		
+		for(String item: lista) {
+			System.out.println("Novos valores:" + item);
+		}
+	}
+	
+	private String obterDados() {
+		try {
+			
+			System.out.println("Digite o valor:");
+			int valor = scanner.nextInt();
+			
+			return String.valueOf(valor);
+			
+		} catch (Exception e) {
+			scanner.next();
+			System.out.println("um valor invalido foi digitado");
+			return "VALOR_INVALIDO";
+		}
 	}
 
 }
